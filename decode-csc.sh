c8ac054bcc6eaad53432522e830f997753843efc
Fredin <90355410+fredinj@users.noreply.github.com>
2022-04-29 21:51:14 +0530

    Update decode-csc.sh

--- decode-csc.sh
+++ decode-csc.sh
@@ -17,8 +17,6 @@ echo -e "-- Downloading OMC Decoder --"
 
 curl --no-progress-meter -Lo cscdecoder-aarch64 https://github.com/soulr344/OMCDecoder/releases/download/v1.0/cscdecoder-aarch64 && chmod +x cscdecoder-aarch64
 
-echo -e "\n"
-
 echo -e "-- Decrypting --\n\n"
 
 ./cscdecoder-aarch64 -i /optics/configs/carriers/$REGION/conf/cscfeature.xml
