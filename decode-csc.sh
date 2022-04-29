c506d784ee74f7e724bf4b091d6ac0525d69b3c8
Fredin <90355410+fredinj@users.noreply.github.com>
2022-04-29 21:48:00 +0530

    Update decode-csc.sh

--- decode-csc.sh
+++ decode-csc.sh
@@ -4,13 +4,19 @@ clear
 
 REGION=`ls /optics/configs/carriers`
 
+echo -e "\n\n Mounting RW"
+
+blockdev --setrw /dev/block/dm-0
+mount -o rw,remount /dev/block/dm-0
+mount -o rw,remount /
+
 echo -e "\n\n-- Making a Backup of cscfeature.xml --\n\n"
 
 cp /optics/configs/carriers/$REGION/conf/cscfeature.xml /optics/configs/carriers/$REGION/conf/cscfeature.xml.bak
 
 echo -e "-- Downloading OMC Decoder --\n\n"
 
-curl -Lo cscdecoder-aarch64 https://github.com/soulr344/OMCDecoder/releases/download/v1.0/cscdecoder-aarch64 && chmod +x cscdecoder-aarch64
+curl --no-progress-meter -Lo cscdecoder-aarch64 https://github.com/soulr344/OMCDecoder/releases/download/v1.0/cscdecoder-aarch64 && chmod +x cscdecoder-aarch64
 
 echo -e "\n"
 
