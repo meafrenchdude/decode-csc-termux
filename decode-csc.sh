48a10de305c802b0a3ba94aa9e652971dcf588e6
Fredin <90355410+fredinj@users.noreply.github.com>
2022-05-07 23:57:56 +0530

    Update decode-csc.sh

--- decode-csc.sh
+++ decode-csc.sh
@@ -12,16 +12,16 @@ else
 pth="/optics/configs/carriers/$region/conf"
 fi
 
-backup=$pth/cscfeature.xml.bak
+backup=$path/cscfeature.xml.bak
 
 echo -e "\n\n-- You are on Android $android --"
 echo -e "\n\n-- Your Region is $region --"
 
-#checking if the files exist
-if [ -d /optics/configs/carriers/$region ] && [ -f $pth/cscfeature.xml ]; then
+#Checking if the files exist
+if [ -d /optics/configs/carriers/$region ] && [ -f $path/cscfeature.xml ]; then
 
 #Checking if it's already decryted
-if [ "`file $pth/cscfeature.xml`" == "$pth/cscfeature.xml: data" ]; then
+if [ "`file $path/cscfeature.xml`" == "$path/cscfeature.xml: data" ]; then
 
 echo -e "\n\n-- Mounting RW --"
 mount -o rw,remount /optics
@@ -29,7 +29,7 @@ mount -o rw,remount /optics
 echo -e "\n\n-- Making a Backup of cscfeature.xml --"
 #Checking if a backup exists
 if [ ! -f "$backup" ]; then
-cp $pth/cscfeature.xml $pth/cscfeature.xml.bak
+cp $path/cscfeature.xml $path/cscfeature.xml.bak
 else
 echo -e "\n\n-- Backup Already Exists --"
 fi
@@ -38,9 +38,9 @@ echo -e "\n\n-- Downloading OMC Decoder --"
 curl --no-progress-meter -Lo cscdecoder-aarch64 https://github.com/soulr344/OMCDecoder/releases/download/v1.0/cscdecoder-aarch64 && chmod +x cscdecoder-aarch64
 
 echo -e "\n\n-- Decrypting --"
-./cscdecoder-aarch64 -i $pth/cscfeature.xml
-chmod 644 $pth/cscfeature.xml
-chmod 644 $pth/cscfeature.xml.bak
+./cscdecoder-aarch64 -i $path/cscfeature.xml
+chmod 644 $path/cscfeature.xml
+chmod 644 $path/cscfeature.xml.bak
 
 echo -e "\n\n-- Done :) --"
 
@@ -50,7 +50,8 @@ echo -e "\n\n-- File is Already Decrypted --"
 fi
 
 else
-echo -e "\n\n-- Can't Find The Required Files at /optics/configs/carriers/$region --"
+echo -e "\n\n-- Can't find the required files at $path --"
+echo -e "\n\n-- Make sure the files are there and you are on the right region of your stock rom --"
 
 fi
 
